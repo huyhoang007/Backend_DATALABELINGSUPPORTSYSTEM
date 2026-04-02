904652e8eb3dd1f693c11f7bcef71a6b37eb91fc
Duy Phan <duyphse161125@fpt.edu.vn>
2026-04-02 14:27:34 +0700

    thêm import

--- src/main/java/com/datalabeling/datalabelingsupportsystem/repository/Policy/ViolationRepository.java
+++ src/main/java/com/datalabeling/datalabelingsupportsystem/repository/Policy/ViolationRepository.java
@@ -3,6 +3,10 @@ package com.datalabeling.datalabelingsupportsystem.repository.Policy;
 import com.datalabeling.datalabelingsupportsystem.pojo.Violation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 @Repository
 public interface ViolationRepository extends JpaRepository<Violation, Long> {
